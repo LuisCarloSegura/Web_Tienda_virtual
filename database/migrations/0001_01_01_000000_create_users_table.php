21eeeb48c44c83e4dbe5588452ad1efb03030fa9
LuisCarloSegura <lsegurarayo@gmail.com>
2026-08-14 08:30:31 -0600

    Se finalizó la migración de la tabla de usuarios incluyendo la columna para el rol de administrador

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            // Campo de rol para distinguir entre 'administrador' y 'cliente' (por defecto 'cliente')
+            $table->string('rol')->default('cliente');
             $table->rememberToken();
             $table->timestamps();
         });
